003d023ba89180fbc244e35df4fd0921e8d6756c
”miracle” <craptysk@gmail.com>
2025-01-30 11:51:08 +0700

    DONE

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,8 +1,10 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
 import web.service.CarServiceImpl;
@@ -12,16 +14,23 @@ import java.util.List;
 
 @Controller
 public class CarController {
+
     @GetMapping(value = "/cars")
-    public String printCars(ModelMap model) {
-        CarService carService = new CarServiceImpl();
-        carService.addCar(new Car("Toyota", 2020, 150.00));
-        carService.addCar(new Car("BMW", 2021, 200.15));
-        carService.addCar(new Car("Nissan", 2025, 300.5));
-        carService.addCar(new Car("Opel", 1999, 0.7));
-        carService.addCar(new Car("Mersedes", 2023, 360.15));
-        carService.addCar(new Car("UAZ", 2010, 100.00));
-        model.addAttribute("messages", carService.showCars(2));
+
+    public String showCars(ModelMap model, @RequestParam(name = "count", required = false, defaultValue = "5") int count) {
+            CarService carService = new CarServiceImpl();
+            carService.addCar(new Car("Toyota", 2020, 150.00));
+            carService.addCar(new Car("BMW", 2021, 200.15));
+            carService.addCar(new Car("Nissan", 2025, 300.5));
+            carService.addCar(new Car("Opel", 1999, 0.7));
+            carService.addCar(new Car("Mersedes", 2023, 360.15));
+            carService.addCar(new Car("UAZ", 2010, 100.00));
+
+        if (count < 0 || count >= 5) {
+            model.addAttribute("cars", carService.showCars(5));
+        } else {
+            model.addAttribute("cars", carService.showCars(count));
+        }
         return "cars";
     }
 
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,24 +1,29 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CarServiceImpl implements CarService {
     private List<Car> cars = new ArrayList<>();
 
     @Override
     public void addCar(Car car) {
-        cars.add(car);
 
+        cars.add(car);
     }
+
 
     @Override
     public List<Car> showCars(int count) {
+
         return cars.stream()
                 .limit(count >= 5 ? cars.size() : count)
                 .toList();
     }
 }
+
 
